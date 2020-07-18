187ad0a4bab5a415b504640f3f1115f7a6028351
Sven Mkw <sven.mkw@gmail.com>
2020-07-18 19:37:38 +0200

    Add basic CLI tool and setup routine

--- feed/cli.py
+++ feed/cli.py
@@ -6,4 +6,30 @@
 #   secret input for password
 # - path for storing diff files
 # - option to not send email on initial crawl
+# - option to attach remote debugger
 # command to clear diff files
+import logging
+from pprint import pformat
+
+from bs4 import BeautifulSoup
+
+from .feed import Feed
+
+
+def parser(dom: BeautifulSoup) -> str:
+    latest_chapter_link = dom.select(
+        "#Chapters_List .ceo_latest_comics_widget li:first-child a"
+    )[0]
+
+    logging.debug(pformat(latest_chapter_link, indent=2))
+    return str(latest_chapter_link)
+
+    # def to_email(dom: BeautifulSoup) -> str:
+    #     raise NotImplementedError
+
+
+def main():
+    OnePunchManFeed = Feed(
+        title="One Punch Man", url="https://ldkmanga.com", parser=parser
+    )
+    OnePunchManFeed.sync_and_send()
--- setup.py
+++ setup.py
@@ -1,2 +1,20 @@
 # register cli tool
 # install invoke
+from setuptools import setup, find_packages
+
+
+setup(
+    name="feed",
+    version="0.0.0.dev",
+    # description=short_description,
+    # long_description=long_description,
+    # url="https://github.com/thinksono/predictor",
+    author="Sven Mischkewitz",
+    author_email="sven.mkw@gmail.com",
+    license="Private License",
+    packages=find_packages(),
+    install_requires=["beautifulsoup4>=4.9.1,<5"],
+    # include_package_data=True,
+    # scripts=scripts,
+    entry_points={"console_scripts": ["feed = feed.cli:main"]},
+)
