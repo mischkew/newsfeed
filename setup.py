dd257b263cb6153f2d5b2199f6e76351a807dd2b
Sven Mkw <sven.mkw@gmail.com>
2020-07-19 19:02:17 +0200

    Update setup.py description and license

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
+short_description = (
+    """Personal email news feed for blogs and other websites which don't provide RSS"""
+)
+long_description = Path("./README.md").read_text("utf8").strip()
 
 setup(
     name="feed",
     version=Path("./VERSION").read_text("utf8").strip(),
-    # description=short_description,
-    # long_description=long_description,
-    # url="https://github.com/thinksono/predictor",
+    description=short_description,
+    long_description=long_description,
+    url="https://github.com/mischkew/newsfeed",
     author="Sven Mischkewitz",
     author_email="sven.mkw@gmail.com",
-    license="Private License",
+    license="MIT License",
     packages=find_packages(),
     install_requires=["beautifulsoup4>=4.9.1,<5"],
     extras_require={"dev": ["ptvsd>=4.3.2,<5"]},
