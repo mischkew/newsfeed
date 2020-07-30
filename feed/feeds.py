810e5259f6ae5b7002e55829d0ea6ff584d9fb5b
Sven Mkw <sven.mkw@gmail.com>
2020-07-30 10:15:04 +0100

    Unify parsing and email building for all feeds

--- feed/feeds.py
+++ feed/feeds.py
@@ -1,13 +1,18 @@
 import logging
 from pprint import pformat
 from typing import List
+from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 
-from .feed import Feed
+from .feed import Feed, Parser, MailBuilder
 
 
 class DuplicateFeed(Exception):
+    pass
+
+
+class NoAnchorTag(Exception):
     pass
 
 
@@ -19,58 +24,83 @@ def ensure_no_duplicates(feeds) -> None:
         seen.add(feed.id)
 
 
-def manga_parser(feed: Feed, dom: BeautifulSoup) -> str:
-    latest_chapter_link = dom.select(
-        "#Chapters_List .ceo_latest_comics_widget li:first-child a"
-    )[0]
-
-    logging.debug(pformat(latest_chapter_link, indent=2))
-    return str(latest_chapter_link)
+BLOG_POST_EMAIL = "A new blog post of {title} is available!"
+MANGA_CHAPTER_EMAIL = "A new chapter of {title} is available!"
+EPISODE_EMAIL = "A new episode of {title} is available!"
 
 
-def build_manga_email(feed: Feed, dom: BeautifulSoup) -> str:
-    email = f"A new chapter of {feed.title} is available!\n"
-    email += str(dom)
-    return email
+def build_parser(pattern: str) -> Parser:
+    """Create a parser from a CSS selector. If the selector returns multiple
+    elements, only the first one is observed for change. The CSS selector should
+    contain a link.
+    """
+
+    def parser(feed: Feed, dom: BeautifulSoup) -> str:
+        element = dom.select(pattern)[0]
+        logging.debug(pformat(element, indent=2))
+
+        if element.name != "a" and element.find("a") is None:
+            raise NoAnchorTag("The parser pattern must contain an anchor-tag!")
+
+        return str(element)
+
+    return parser
 
 
-def clean_code_parser(feed: Feed, dom: BeautifulSoup) -> str:
-    latest_post = dom.select("aside ul li:first-child a")[0]
+def build_mail_builder(prefix: str) -> MailBuilder:
+    """Creates an email builder from a text which is prepended to the parsed DOM
+    element. If the parsed DOM element is a relative link, the domain url will
+    be prepended to convert it into an absolute link (which can be visited from
+    the email client of the recipient).
+    """
 
-    logging.debug(pformat(latest_post, indent=2))
-    return str(latest_post)
+    def mail_builder(feed: Feed, dom: BeautifulSoup) -> str:
+        link = dom.select("a")[0]
+        link["href"] = urljoin(feed.url, link["href"])
 
+        email = prefix.format(**feed._asdict())
+        email += "\n"
+        email += str(dom)
+        return email
 
-def build_clean_code_email(feed: Feed, dom: BeautifulSoup) -> str:
-    # uncle bob's blog uses relative links, so we prepend the webpage url
-    link = dom.select("a")[0]
-    link["href"] = feed.url + link["href"]
-
-    email = f"A new blog post of {feed.title} is available!\n"
-    email += str(link)
-    return email
+    return mail_builder
 
 
 OnePunchManFeed = Feed(
     title="One Punch Man",
     url="https://ldkmanga.com",
-    parser=manga_parser,
-    build_email=build_manga_email,
+    parser=build_parser("#Chapters_List .ceo_latest_comics_widget li:first-child a"),
+    build_email=build_mail_builder(MANGA_CHAPTER_EMAIL),
 )
 OnePieceFeed = Feed(
     title="One Piece",
     url="https://onepiece-mangaonline.com/",
-    parser=manga_parser,
-    build_email=build_manga_email,
+    parser=build_parser("#Chapters_List .ceo_latest_comics_widget li:first-child a"),
+    build_email=build_mail_builder(MANGA_CHAPTER_EMAIL),
 )
 CleanCodeFeed = Feed(
     title="Clean Code Blog",
     url="https://blog.cleancoder.com/",
-    parser=clean_code_parser,
-    build_email=build_clean_code_email,
+    parser=build_parser("aside ul li:first-child a"),
+    build_email=build_mail_builder(BLOG_POST_EMAIL),
+)
+HinowaGaYukuFeed = Feed(
+    title="Hinowa Ga Yuku",
+    url="https://mangakakalot.com/read-lt3yx158504948647",
+    parser=build_parser("#chapter .chapter-list .row:first-child a"),
+    build_email=build_mail_builder(MANGA_CHAPTER_EMAIL),
+)
+EnenNoShouboutai = Feed(
+    title="Enen No Shouboutai Season 2",
+    url="https://www11.gogoanimehub.com/enen-no-shouboutai-ni-no-shou-episode-1-1",
+    parser=build_parser(".episodes li:first-child a"),
+    build_email=build_mail_builder(EPISODE_EMAIL),
 )
 
-FEEDS = sorted([OnePunchManFeed, OnePieceFeed, CleanCodeFeed], key=lambda f: f.id)
+FEEDS = sorted(
+    [OnePunchManFeed, OnePieceFeed, CleanCodeFeed, HinowaGaYukuFeed, EnenNoShouboutai],
+    key=lambda f: f.id,
+)
 FEED_ID_INDEX = {feed.id: feed for feed in FEEDS}
 ensure_no_duplicates(FEEDS)
 
